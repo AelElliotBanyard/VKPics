d3e441d7077c597bbb9f505a25a25f41a574d750
aelbanyard <ael.banyard@lernende.bbw.ch>
2023-06-23 15:29:36 +0200

    add content for landing page

--- CODE/app/page.tsx
+++ CODE/app/page.tsx
@@ -1,3 +1,11 @@
 export default function Home() {
-  return <div></div>;
+  return (
+    <div className="landingpage">
+      <div className="landingpage__quote__container">
+        <h1 className="landingpage__quote">
+          “Don't stand in the middle of traffic, you'll harm yourself.”
+        </h1>
+      </div>
+    </div>
+  );
 }
