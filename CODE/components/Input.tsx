9dd70c5d7677fba5a161aa5595329c87bfc5fb89
aelbanyard <ael.banyard@lernende.bbw.ch>
2023-06-23 15:55:51 +0200

    Update Input.tsx

--- CODE/components/Input.tsx
+++ CODE/components/Input.tsx
@@ -1,5 +1,17 @@
-const Input = () => {
-  return <div></div>;
+import { InputProps } from "@/types";
+
+const Input = ({ label, value, onChange, type }: InputProps) => {
+  return (
+    <div className="input__container">
+      <label className="input__label">{label}</label>
+      <input
+        className="input__input"
+        type={type ? type : "text"}
+        value={value}
+        onChange={(e) => onChange(e.target.value)}
+      />
+    </div>
+  );
 };
 
 export default Input;
