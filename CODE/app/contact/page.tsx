ec26a6f0c881a6fcd1eb8047b554e5744619bb25
aelbanyard <ael.banyard@lernende.bbw.ch>
2023-06-23 15:56:51 +0200

    Update page.tsx

--- CODE/app/contact/page.tsx
+++ CODE/app/contact/page.tsx
@@ -1,5 +1,15 @@
-const page = () => {
-  return <div></div>;
+"use client";
+import ContactForm from "@/components/ContactForm";
+
+const Contact = () => {
+  return (
+    <div className="contact">
+      <h1 className="contact__title">Contact Us</h1>
+      <div className="contact__form">
+        <ContactForm />
+      </div>
+    </div>
+  );
 };
 
-export default page;
+export default Contact;
