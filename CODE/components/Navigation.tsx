853aa8dd14cb9dee53540902ad3a6e6ddfce39aa
aelbanyard <ael.banyard@lernende.bbw.ch>
2023-06-23 15:33:14 +0200

    Update Navigation.tsx

--- CODE/components/Navigation.tsx
+++ CODE/components/Navigation.tsx
@@ -1,5 +1,56 @@
+"use client";
+
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+
 const Navigation = () => {
-  return <div></div>;
+  const current = usePathname().split("/")[1];
+  return (
+    <div className="navigation">
+      <h1 className="navigation__title">
+        <Link href="/">VKPics</Link>
+      </h1>
+      <div className="navigation__links">
+        <Link
+          href="/"
+          className={"navigation__link " + (current === "" ? "active" : "")}
+        >
+          Home
+        </Link>
+        <Link
+          href="/offers"
+          className={
+            "navigation__link " + (current === "offers" ? "active" : "")
+          }
+        >
+          Offers
+        </Link>
+        <Link
+          href="/about"
+          className={
+            "navigation__link " + (current === "about" ? "active" : "")
+          }
+        >
+          About Us
+        </Link>
+        <Link
+          href="/work"
+          className={"navigation__link " + (current === "work" ? "active" : "")}
+        >
+          Our Projects
+        </Link>
+        <Link
+          href="/contact"
+          className={
+            "navigation__link__button" +
+            (current === "contact" ? " active" : "")
+          }
+        >
+          Contact Us
+        </Link>
+      </div>
+    </div>
+  );
 };
 
 export default Navigation;
