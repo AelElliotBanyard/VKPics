cf081cb6a776c3a18cba301a97b9f21e6f73f1d3
aelbanyard <ael.banyard@lernende.bbw.ch>
2023-06-23 15:56:34 +0200

    Update ContactForm.tsx

--- CODE/components/ContactForm.tsx
+++ CODE/components/ContactForm.tsx
@@ -1,5 +1,43 @@
+"use client";
+import { useState } from "react";
+import Input from "./Input";
+
 const ContactForm = () => {
-  return <div></div>;
+  const [firstname, setFirstname] = useState("");
+  const [lastname, setLastname] = useState("");
+  const [email, setEmail] = useState("");
+  const [message, setMessage] = useState("");
+
+  return (
+    <form className="contactForm">
+      <div className="contactForm__left">
+        <Input
+          label="Firstname"
+          value={firstname}
+          onChange={(e: string) => setFirstname(e)}
+        />
+        <Input
+          label="Lastname"
+          value={lastname}
+          onChange={(e: string) => setLastname(e)}
+        />
+        <Input
+          label="E-Mail"
+          value={email}
+          type="email"
+          onChange={(e: string) => setEmail(e)}
+        />
+      </div>
+      <div className="contactForm__right">
+        <Input
+          label="Message"
+          value={message}
+          onChange={(e: string) => setMessage(e)}
+        />
+        <button className="contactForm__button">Send</button>
+      </div>
+    </form>
+  );
 };
 
 export default ContactForm;
