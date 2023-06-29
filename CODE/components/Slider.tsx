958ce74e156d548f91a2c609d6b65a20fd83a374
aelbanyard <ael.banyard@lernende.bbw.ch>
2023-06-29 13:03:17 +0200

    create slider component

--- CODE/components/Slider.tsx
+++ CODE/components/Slider.tsx
@@ -1,11 +1,17 @@
-import React from 'react';
+import { SliderProps } from "@/types";
+import React from "react";
+import {
+  ReactCompareSlider,
+  ReactCompareSliderImage,
+} from "react-compare-slider";
 
-const Slider = () => {
-    return (
-        <div>
-            
-        </div>
-    );
+const Slider = ({ img1, img2 }: SliderProps) => {
+  return (
+    <ReactCompareSlider
+      itemOne={<ReactCompareSliderImage src={img1} alt="before" />}
+      itemTwo={<ReactCompareSliderImage src={img2} alt="after" />}
+    />
+  );
 };
 
 export default Slider;
