6c4c46824fbe8a2e462c31496032be35b4a61a7b
aelbanyard <ael.banyard@lernende.bbw.ch>
2023-06-30 10:27:52 +0200

    add content to footer

--- CODE/components/Footer.tsx
+++ CODE/components/Footer.tsx
@@ -1,7 +1,31 @@
-import React from "react";
+"use client";
+
+import Link from "next/link";
 
 const Footer = () => {
-  return <div></div>;
+  return (
+    <div className="footer">
+      <h1 className="footer__title">VKPics</h1>
+      <div className="footer__links">
+        <Link href="/">Home</Link>
+        <Link href="/offers" className="footer__link">
+          Offers
+        </Link>
+        <Link href="/about" className="footer__link">
+          About Us
+        </Link>
+        <Link href="/work" className="footer__link">
+          My Work
+        </Link>
+        <Link href="/contact" className="footer__link">
+          Contact Us
+        </Link>
+        <Link href="/impressum" className="footer__link">
+          Impressum
+        </Link>
+      </div>
+    </div>
+  );
 };
 
 export default Footer;
