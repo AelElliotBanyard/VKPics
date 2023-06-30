9976c9cca03aa77b28f6053c17bc285c6bf58b7a
aelbanyard <ael.banyard@lernende.bbw.ch>
2023-06-30 09:59:31 +0200

    add layout to route group

---
+++ CODE/app/(newLayout)/layout.tsx
@@ -0,0 +1,19 @@
+import Navigation from "@/components/Navigation";
+import { Inter } from "next/font/google";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>
+        <Navigation />
+        <div className="background">{children}</div>
+      </body>
+    </html>
+  );
+}
